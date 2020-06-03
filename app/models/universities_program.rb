2167ae249c5b1397eff4995efdd81e99ab13d5bb
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-06-03 16:52:22 +0200

    pashfp

--- app/models/universities_program.rb
+++ app/models/universities_program.rb
@@ -2,4 +2,6 @@ class UniversitiesProgram < ApplicationRecord
     belongs_to :program 
     belongs_to :university
     has_many :applications
+    has_many :universities_programs_documents
+    has_many :documents, through: :universities_programs_documents
 end
