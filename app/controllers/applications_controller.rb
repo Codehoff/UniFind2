a312732b57fb3a3961b5f0ccdce2375c99f9c1cb
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-06-07 19:33:46 +0200

    testing the calendar

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,11 +4,15 @@ class ApplicationsController < ApplicationController
 
   def index
     @applications = Application.where(user: current_user)
+    @universities_programs_done = []
     @universities_programs = []
-    @applications.each do |app| 
-      @universities_programs << app.universities_program
+    @applications.each do |app|
+      if app.universities_program.documents - app.documents == []
+        @universities_programs_done << app.universities_program
+      else
+        @universities_programs << app.universities_program
+      end
     end
-
   end
 
   def show
