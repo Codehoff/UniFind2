b804d48d22fc4123d3d7387c584c8cec45d42098
Felix Meumann <f.meumann@gmail.com>
2020-06-09 11:14:57 +0200

    Add picture to UniversitiesProgram

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_150652) do
+ActiveRecord::Schema.define(version: 2020_06_09_091236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_150652) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "program_id"
     t.bigint "university_id"
+    t.string "picture"
     t.index ["program_id"], name: "index_universities_programs_on_program_id"
     t.index ["university_id"], name: "index_universities_programs_on_university_id"
   end
