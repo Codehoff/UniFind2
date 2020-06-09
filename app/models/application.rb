fc3b6b3d9fd3a78f7e23fe811acb6bc83d5aee33
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-06-09 11:54:24 +0200

    123

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,10 @@
 class Application < ApplicationRecord
-    has_many :applications_documents
-    has_many :documents, through: :applications_documents
+    has_many :applications_documents, dependent: :destroy
+    has_many :documents, through: :applications_documents, dependent: :destroy
     belongs_to :universities_program
     belongs_to :user
     has_one :university, through: :universities_program
     has_one :program, through: :universities_program
-    has_many :notifications
+    has_many :notifications, dependent: :destroy
+    validates :universities_program_id, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :applications
+  has_many :applications, dependent: :destroy
   has_many :universities_programs, through: :applications
+  has_many :notifications, through: :applications
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
