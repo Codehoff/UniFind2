e18947eb50cb6a39ffc130c27b7d0be59fbc6269
Donata Dietz <dietzdonata@gmail.com>
2020-06-09 11:18:40 +0200

    js

---
+++ app/javascript/packs/hidden-search.js
@@ -0,0 +1,13 @@
+const hiddenSearch = () => {
+  const searchQuery = document.getElementById("hidden-search");
+  const closeX = document.getElementById("close");
+  const hiddenSearchbox = document.getElementById("hidden-search");
+  searchQuery.addEventListener('focus', () => {
+    hiddenSearchbox.style.display = "block";
+  });
+  closeX.addEventListener('click', () => {
+    hiddenSearchbox.style.display = "none";
+  });
+}
+
+export { hiddenSearch };
