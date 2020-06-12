d8ac3a88eb6ba526cdfb35c8f0009e4672dee9c2
enno kirchhoff <enno.kirchhoff1911@gmail.com>
2020-06-12 12:53:12 +0200

    dasd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  
+
   before_action :new_notification, only: [:home]
   skip_before_action :authenticate_user!, only: [ :home ]
 
@@ -9,21 +9,21 @@ class PagesController < ApplicationController
   def new_notification
     if user_signed_in?
       @user_apps = Application.joins(:universities_program).order("start_time ASC").where(user_id: current_user.id)
-      @user_programs = [] 
+      @user_programs = []
       @user_apps.each do |app|
         @user_programs << app.universities_program
-      end    
+      end
       @user_apps.each do |app|
         if (Time.now - current_user.current_sign_in_at).floor <= 10
-          if (app.universities_program.documents - app.documents) != [] 
+          if (app.universities_program.documents - app.documents) != []
             if ((app.universities_program.start_time - Time.now)/86400) <= 30
               if app.notifications == []
                 Notification.create(application_id: app.id, content:"Only #{((app.universities_program.start_time - Time.now)/86400).ceil} days left for your application at #{app.universities_program.university.name}")
-              end 
+              end
             end
           end
         end
       end
     end
-  end 
+  end
 end
